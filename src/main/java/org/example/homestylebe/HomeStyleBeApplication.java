ecac3e0edfe7f895934b7aaf3852ea8445bc1729
raffaeletartaglia <trtrfl98s29g317z@studenti.unical.it>
2026-07-02 18:39:43 +0200

    Initial commit

---
+++ src/main/java/org/example/homestylebe/HomeStyleBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.homestylebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStyleBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeStyleBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/homestylebe/HomeStyleBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homestylebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStyleBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
